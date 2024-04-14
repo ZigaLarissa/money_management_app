e4b3117f2fb5de9c5354662ba93fda2a71963e62
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-04-14 21:03:45 +0200

    Signup page ui updated

--- cashapp/lib/screens/signup.dart
+++ cashapp/lib/screens/signup.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/widgets.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -18,160 +15,235 @@ class _SignUpState extends State<SignUp> {
       backgroundColor: const Color(0xFF0B0742),
       body: Container(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 70),
-            const Text(
-              'Sign Up',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 140),
+              const Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Form(
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          style: const TextStyle(
-                              color: Color(0xFFD2CECE), fontSize: 14),
-                          decoration: const InputDecoration(
-                            labelText: 'First Name',
-                            labelStyle: TextStyle(color: Color(0xFFD2CECE)),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                              borderSide:
-                                  BorderSide(color: Colors.white, width: 2),
+              const SizedBox(height: 30),
+              Form(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            style: const TextStyle(
+                                color: Color(0xFFD2CECE), fontSize: 14),
+                            decoration: const InputDecoration(
+                              labelText: 'First Name',
+                              labelStyle: TextStyle(color: Color(0xFFD2CECE)),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                borderSide:
+                                    BorderSide(color: Colors.white, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                borderSide:
+                                    BorderSide(color: Color(0xFFFF9190)),
+                              ),
+                              contentPadding: EdgeInsets.only(left: 20),
+                              constraints:
+                                  BoxConstraints(maxHeight: 45, minHeight: 45),
                             ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                              borderSide: BorderSide(color: Color(0xFFFF9190)),
-                            ),
-                            contentPadding: EdgeInsets.only(left: 20),
-                            constraints:
-                                BoxConstraints(maxHeight: 43, minHeight: 43),
                           ),
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        child: TextFormField(
-                          style: const TextStyle(
-                              color: Color(0xFFD2CECE), fontSize: 14),
-                          decoration: const InputDecoration(
-                            labelText: 'Last Name',
-                            labelStyle: TextStyle(color: Color(0xFFD2CECE)),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                              borderSide:
-                                  BorderSide(color: Colors.white, width: 2),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          child: TextFormField(
+                            style: const TextStyle(
+                                color: Color(0xFFD2CECE), fontSize: 14),
+                            decoration: const InputDecoration(
+                              labelText: 'Last Name',
+                              labelStyle: TextStyle(color: Color(0xFFD2CECE)),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                borderSide:
+                                    BorderSide(color: Colors.white, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(50)),
+                                borderSide:
+                                    BorderSide(color: Color(0xFFFF9190)),
+                              ),
+                              contentPadding: EdgeInsets.only(left: 20),
+                              constraints:
+                                  BoxConstraints(maxHeight: 45, minHeight: 45),
                             ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(50)),
-                              borderSide: BorderSide(color: Color(0xFFFF9190)),
-                            ),
-                            contentPadding: EdgeInsets.only(left: 20),
-                            constraints:
-                                BoxConstraints(maxHeight: 43, minHeight: 43),
                           ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(
+                          color: Color(0xFFD2CECE), fontSize: 14),
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        labelStyle: TextStyle(color: Color(0xFFD2CECE)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Color(0xFFFF9190)),
+                        ),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        constraints:
+                            BoxConstraints(maxHeight: 45, minHeight: 45),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(
+                          color: Color(0xFFD2CECE), fontSize: 14),
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(color: Color(0xFFD2CECE)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Color(0xFFFF9190)),
+                        ),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        constraints:
+                            BoxConstraints(maxHeight: 45, minHeight: 45),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      style: const TextStyle(
+                          color: Color(0xFFD2CECE), fontSize: 14),
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm Password',
+                        labelStyle: TextStyle(color: Color(0xFFD2CECE)),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Colors.white, width: 2),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
+                          borderSide: BorderSide(color: Color(0xFFFF9190)),
+                        ),
+                        contentPadding: EdgeInsets.only(left: 20),
+                        constraints:
+                            BoxConstraints(maxHeight: 45, minHeight: 45),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              CheckboxListTile(
+                value: timeDilation != 1.0,
+                contentPadding: const EdgeInsets.all(0),
+                activeColor: const Color(0xFFFF9190),
+                checkColor: Colors.white,
+                fillColor: MaterialStateProperty.all(const Color(0xFFFF9190)),
+                checkboxShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                onChanged: (bool? value) {
+                  setState(() {
+                    timeDilation = value! ? 2.0 : 1.0;
+                  });
+                },
+                title: RichText(
+                  text: const TextSpan(
+                    text: 'I agree with ',
+                    style: TextStyle(color: Color(0xFFD2CECE)),
+                    children: [
+                      TextSpan(
+                        text: 'privacy',
+                        style: TextStyle(
+                          color: Color(0xFFFF9190),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' and ',
+                        style: TextStyle(color: Color(0xFFD2CECE)),
+                      ),
+                      TextSpan(
+                        text: 'policy',
+                        style: TextStyle(
+                          color: Color(0xFFFF9190),
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ],
                   ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    style:
-                        const TextStyle(color: Color(0xFFD2CECE), fontSize: 14),
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      labelStyle: TextStyle(color: Color(0xFFD2CECE)),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Colors.white, width: 2),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Color(0xFFFF9190)),
-                      ),
-                      contentPadding: EdgeInsets.only(left: 20),
-                      constraints: BoxConstraints(maxHeight: 43, minHeight: 43),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    style:
-                        const TextStyle(color: Color(0xFFD2CECE), fontSize: 14),
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: TextStyle(color: Color(0xFFD2CECE)),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Colors.white, width: 2),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Color(0xFFFF9190)),
-                      ),
-                      contentPadding: EdgeInsets.only(left: 20),
-                      constraints: BoxConstraints(maxHeight: 43, minHeight: 43),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  TextFormField(
-                    style:
-                        const TextStyle(color: Color(0xFFD2CECE), fontSize: 14),
-                    decoration: const InputDecoration(
-                      labelText: 'Confirm Password',
-                      labelStyle: TextStyle(color: Color(0xFFD2CECE)),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Colors.white, width: 2),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)),
-                        borderSide: BorderSide(color: Color(0xFFFF9190)),
-                      ),
-                      contentPadding: EdgeInsets.only(left: 20),
-                      constraints: BoxConstraints(maxHeight: 43, minHeight: 43),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                ],
+                ),
+                controlAffinity: ListTileControlAffinity.leading,
               ),
-            ),
-            CheckboxListTile(
-              value: timeDilation != 1.0,
-              onChanged: (bool? value) {
-                setState(() {
-                  timeDilation = value! ? 2.0 : 1.0;
-                });
-              },
-              title: const Text('I agree with privacy and policy'),
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Sign Up'),
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: const Text('Already have an account? Sign In'),
-            ),
-          ],
+              const SizedBox(height: 100),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(vertical: 3),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFFFF9190), Color(0xFF5E72EB)],
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 100),
+              Container(
+                alignment: Alignment.center,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: RichText(
+                    text: const TextSpan(
+                      text: 'Already have an account? ',
+                      style: TextStyle(color: Color(0xFFD2CECE)),
+                      children: [
+                        TextSpan(
+                          text: 'Login',
+                          style: TextStyle(
+                            color: Color(0xFFFF9190),
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
